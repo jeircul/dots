ca14efd0cb0552b06e05b70ff2c81cf7649f058f
Ove Aursland <jeircul@mailbox.org>
2023-01-02 10:58:59 +0100

    added explorer.lua

---
+++ .config/nvim/lua/explorer.lua
@@ -0,0 +1,32 @@
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fr', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>fc', builtin.commands, {})
+
+-- nnn
+local builtin = require("nnn").builtin
+require("nnn").setup({
+	picker = {
+		fullscreen = false,
+		cmd = "tmux new-session nnn -Pp",
+		style = { border = "rounded" },
+		session = "shared",
+	},
+	explorer = {
+		fullscreen = false,
+	},
+	replace_netrw = "picker",
+	mappings = {
+		{ "<C-t>", builtin.open_in_tab },       -- open file(s) in tab
+		{ "<C-s>", builtin.open_in_split },     -- open file(s) in split
+		{ "<C-v>", builtin.open_in_vsplit },    -- open file(s) in vertical split
+		{ "<C-p>", builtin.open_in_preview },   -- open file in preview split keeping nnn focused
+		{ "<C-y>", builtin.copy_to_clipboard }, -- copy file(s) to clipboard
+		{ "<C-w>", builtin.cd_to_path },        -- cd to file directory
+	}
+})
