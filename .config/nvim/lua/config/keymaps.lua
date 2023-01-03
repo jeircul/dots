848c188ad4c059409c7353dfde1643f40b2cfe0d
Ove Aursland <jeircul@mailbox.org>
2023-01-03 22:02:19 +0100

    fix keymaps for nnn

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- navigate buffers
-vim.keymap.set('n', '<TAB>', ':bnext<cr>')
-vim.keymap.set('n', '<S-TAB>', ':bprev<cr>')
+vim.keymap.set("n", "<TAB>", ":bnext<cr>")
+vim.keymap.set("n", "<S-TAB>", ":bprev<cr>")
 
 -- Move Lines
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
@@ -54,20 +54,8 @@ vim.keymap.set("n", "<leader>lg", function()
 end, { desc = "Lazygit" })
 
 -- nnn
-vim.keymap.set({ "n", "t" }, "<C-n>", "<CMD>NnnPicker %:p:h<CR>")
-vim.keymap.set({ "n", "t" }, "<C-e>", "<CMD>NnnExplorer %:p:h<CR>")
--- local builtin = require("nnn").builtin
--- require("nnn").setup({
---   mappings = {
---     { "<C-t>", builtin.open_in_tab }, -- open file(s) in tab
---     { "<C-s>", builtin.open_in_split }, -- open file(s) in split
---     { "<C-v>", builtin.open_in_vsplit }, -- open file(s) in vertical split
---     { "<C-p>", builtin.open_in_preview }, -- open file in preview split keeping nnn focused
---     { "<C-y>", builtin.copy_to_clipboard }, -- copy file(s) to clipboard
---     { "<C-w>", builtin.cd_to_path }, -- cd to file directory
---     { "<C-e>", builtin.populate_cmdline }, -- populate cmdline (:) with file(s)
---   },
--- })
+vim.keymap.set("n", "<C-n>", ":NnnPicker %:p:h<CR>")
+vim.keymap.set("n", "<C-e>", ":NnnExplorer %:p:h<CR>")
 
 -- tmux navigator
 vim.keymap.set({ "n", "t" }, "<C-h>", "<CMD>NavigatorLeft<CR>")
