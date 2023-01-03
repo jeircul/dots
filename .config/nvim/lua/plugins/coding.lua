573d8a48f1973a81891e1d352d659a1bd2b021c7
Ove Aursland <jeircul@mailbox.org>
2023-01-03 23:42:23 +0100

    added sniprun

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,5 +1,37 @@
 return {
 
+  -- SnipRun
+  {
+    -- Sniprun is a code runner plugin for neovim written in Lua and Rust.
+    "michaelb/sniprun",
+    build = "bash ./install.sh",
+    lazy = true,
+    init = function()
+      local opt = { silent = true }
+      vim.keymap.set("n", "<leader>r", function()
+        require("sniprun").run()
+      end, opt)
+      vim.keymap.set("n", "<leader>rr", function()
+        require("sniprun").run("n")
+      end, opt)
+      vim.keymap.set("v", "<leader>r", function()
+        require("sniprun").run("v")
+      end, opt)
+    end,
+
+    config = function()
+      require("sniprun").setup({
+        display = {
+          "NvimNotify",
+        },
+
+        display_options = {
+          notification_timeout = 10000,
+        },
+      })
+    end,
+  },
+
   -- snippets
   {
     "L3MON4D3/LuaSnip",
