e1c3f7302c56a0af2e6136df60e9fe4bcd1ea09d
Ove Aursland <jeircul@mailbox.org>
2023-01-05 10:12:46 +0100

    added vimwiki

---
+++ .config/nvim/lua/lazyvim/plugins/vimwiki.lua
@@ -0,0 +1,21 @@
+return {
+
+  -- vimwiki
+  {
+    "vimwiki/vimwiki",
+    cmd = "VimwikiIndex",
+    keys = { "<leader>w" },
+    init = function()
+      vim.g.vimwiki_list = {
+        { path = "~/vimwiki/", syntax = "markdown", ext = ".md" },
+      }
+      vim.g.vimwiki_conceallevel = 0
+      vim.g.vimwiki_use_calendar = 1
+      vim.g.vimwiki_hl_headers = 1
+      vim.g.vimwiki_hl_cb_checked = 1
+      vim.g.vimwiki_autowriteall = 0
+      -- vim.g.vimwiki_map_prefix = "<F12>"
+      vim.g.vimwiki_table_mappings = 0
+    end,
+  },
+}
