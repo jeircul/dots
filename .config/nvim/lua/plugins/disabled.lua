465f406c3514da7bc3f6bd5c390a910b836e8ed7
Ove Aursland <jeircul@mailbox.org>
2023-02-18 19:39:12 +0100

    Remove bufferline. "gt" mapping disabled and back to normal.

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,13 @@
 -- disable alpha
 return {
   { "goolord/alpha-nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- disable a keymap
+      keys[#keys + 1] = { "gt", false }
+    end,
+  },
 }
