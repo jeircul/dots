d14f7890f07891ed0c023bc98eb1d3e7f9de5eb8
Ove Aursland <jeircul@mailbox.org>
2023-08-27 21:14:10 +0200

    improved script

--- .config/autoinstall/setup_fedora38_qtile_from_gnome_ws.sh
+++ .config/autoinstall/setup_fedora38_qtile_from_gnome_ws.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# Run script
-# curl -sL https://raw.githubusercontent.com/jeircul/dots/main/.config/autoinstall/setup_fedora38_qtile_from_gnome_ws.sh | bash
-
-# Download install.sh
-# curl -sOL https://raw.githubusercontent.com/jeircul/dots/main/install.sh
-
-PLAYBOOK_PATH="$HOME/.config/ansible/playbooks"
-
 command_exists() {
 	command -v "$@" >/dev/null 2>&1
 }
@@ -18,18 +10,19 @@ install_package() {
 	fi
 }
 
-download_playbook() {
-	if ! test -f "$PLAYBOOK_PATH/configure_fedora_qtile.yml"; then
-		echo "Ansible playbook does not exists, downloading..."
-		curl -sL https://raw.githubusercontent.com/jeircul/dots/main/.config/autoinstall/configure_fedora_qtile.yml --output-dir "$HOME/.config/ansible/playbooks/configure_fedora_qtile/yml"
+clone_repo() {
+	if ! test -d "$HOME/git/dots/"; then
+		mkdir -p "$HOME/git"
+		git clone https://github.com/jeircul/dots "$HOME/git/dots"
 	fi
 }
 
 main() {
+	install_package git
 	install_package ansible
 	install_package gh
-	download_playbook
-	ansible-playbook "$PLAYBOOK_PATH/configure_fedora_qtile.yml" -K
+	clone_repo
+	ansible-playbook "$HOME/git/.config/autoinstall/configure_fedora_qtile.yml" -K
 }
 
 main
