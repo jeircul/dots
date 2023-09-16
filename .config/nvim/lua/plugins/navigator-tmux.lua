10094411e528bc22433893b05c8a040cb4c43d0d
Ove Aursland <jeircul@mailbox.org>
2023-09-16 11:54:34 +0200

    moved to dotfiles

--- .config/nvim/lua/plugins/navigator-tmux.lua
+++
@@ -1,9 +0,0 @@
-return {
-
-  -- navigator for tmux
-  {
-    "numToStr/Navigator.nvim",
-    cmd = { "NavigatorLeft", "NavigatorRight", "NavigatorUp", "NavigatorDown", "NavigatorPrevious" },
-    config = true,
-  },
-}
--- .config/nvim/lua/plugins/sniprun.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "michaelb/sniprun",
-    keys = {},
-    opts = {},
-    build = "bash ./install.sh 1",
-    cmd = "SnipRun",
-  },
-}
